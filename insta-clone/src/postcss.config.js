ea61283b387e1fa097d741555d3cb32bcf0c165f
ShahSau <shahriarksaurov@gmail.com>
2021-08-02 15:57:53 +0300

    login page

---
+++ insta-clone/src/postcss.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable global-require */
+
+module.exports = {
+    // eslint-disable-next-line prettier/prettier
+    plugins: [require('tailwindcss'), require('autoprefixer')]
+  };
+  
