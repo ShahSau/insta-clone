2994d34d98055bc785bbbf5383ecfc092855c320
ShahSau <shahriarksaurov@gmail.com>
2021-08-02 21:13:58 +0300

    update

--- insta-clone/src/pages/dashboard.js
+++ insta-clone/src/pages/dashboard.js
@@ -7,7 +7,7 @@ import useUser from '../hooks/use-user';
 import LoggedInUserContext from '../context/logged-in-user';
 
 export default function Dashboard({ user: loggedInUser }) {
-const { user, setActiveUser } = useUser(loggedInUser.uid);
+  const { user, setActiveUser } = useUser(loggedInUser.uid);
   useEffect(() => {
     document.title = 'Instagram';
   }, []);
@@ -15,16 +15,15 @@ const { user, setActiveUser } = useUser(loggedInUser.uid);
   return (
     <LoggedInUserContext.Provider value={{ user, setActiveUser }}>
       <div className="bg-gray-background">
-        {/* <Header /> */}
+        <Header />
         <div className="grid grid-cols-3 gap-4 justify-between mx-auto max-w-screen-lg">
-          {/* <Timeline /> */}
-          {/* <Sidebar /> */}
+          <Timeline />
+          <Sidebar />
         </div>
       </div>
     </LoggedInUserContext.Provider>
   );
 }
-
 Dashboard.propTypes = {
   user: PropTypes.object.isRequired
 };
